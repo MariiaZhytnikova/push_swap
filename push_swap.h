5f1803b006d62fe7858cbdc92ce1c270a3b86033
mzhitnik <119492493+MariiaZhytnikova@users.noreply.github.com>
2024-12-04 19:36:17 +0200

    Update push_swap.h

--- push_swap.h
+++ push_swap.h
@@ -17,10 +17,10 @@ void swap(t_stack *stack);
 void push(t_stack *stack_from, t_stack *stack_to);
 void rotate(t_stack *stack);
 void reverse_rotate(t_stack *stack);
-void sort_three(t_stack *stack);
-void sort_five(t_stack *stack_a, t_stack *stack_b);
-void sort_big(t_stack *stack_a, t_stack *stack_b);
+void sort_three(t_stack *stack); // < 3 perebor 4?
+void sort_five(t_stack *stack_a, t_stack *stack_b); // 5-100 // chunking
+void sort_big(t_stack *stack_a, t_stack *stack_b); // > 100 // Radix sort
 void free_stack(t_stack *stack);
-int parse_args(char **argv, t_stack *stack);
+int parse_args(char **argv, t_stack *stack); // check
 
 #endif
