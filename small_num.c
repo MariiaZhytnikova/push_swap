153111ca143d16a5ede85f4bef5bbaffabde1383
mzhitnik <119492493+MariiaZhytnikova@users.noreply.github.com>
2024-12-04 19:38:00 +0200

    Update small_num.c

--- small_num.c
+++ small_num.c
@@ -10,6 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+void push_swap(t_stack *stack_a, t_stack *stack_b) {
+    int size = stack_size(stack_a);
+
+    if (size <= 1)
+        return;
+    else if (size == 2)
+        sort_2_elements(stack_a);
+    else if (size == 3)
+        sort_3_elements(stack_a);
+    else if (size <= 5)
+        sort_4_5_elements(stack_a, stack_b);
+    else if (size <= 100)
+        sort_chunking(stack_a, stack_b, 20); // Разделите на чанки по 20 элементов
+    else
+        sort_radix(stack_a, stack_b);
+}
 /*
 Algorithm/Sort Checklist
 
