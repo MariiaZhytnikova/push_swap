635091077b2f08006107de90b9f9b97f5df24c9c
mzhitnik <119492493+MariiaZhytnikova@users.noreply.github.com>
2024-12-04 15:46:50 +0200

    Add files via upload

---
+++ small_num.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   small_num.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzhitnik <mzhitnik@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/04 15:43:26 by mzhitnik          #+#    #+#             */
+/*   Updated: 2024/12/04 15:43:37 by mzhitnik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Algorithm/Sort Checklist
+
+Check if the order of the list is correct or if it should be sorted
+
+Make a small algorithm for small numbers (5 and less)
+
+Implement a condition for 2 numbers
+
+Create an algorithm for 3 numbers
+
+Create an algorithm for 4 numbers
+
+    Create an algorithm for 5 numbers
+
+Create another algorithm for all other numbers
+
+
+*/
