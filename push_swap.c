a6fdba902334b6b3cc10cf31ea62e5561ac5779b
mzhitnik <119492493+MariiaZhytnikova@users.noreply.github.com>
2024-12-29 18:32:54 +0200

    Update push_swap.c

--- push_swap.c
+++ push_swap.c
@@ -60,7 +60,7 @@ static void ft_sort_five(t_stack *stack_a, t_stack *stack_b)
 	}
 }
 
-void	ft_sort_it(t_stack *stack_a, t_stack *stack_b)
+/*void	ft_sort_it(t_stack *stack_a, t_stack *stack_b)
 {
 	int	index;
 
@@ -78,6 +78,48 @@ void	ft_sort_it(t_stack *stack_a, t_stack *stack_b)
 		rotate_min_max(stack_b, stack_a, index, 1);
 		ft_pa_pb(stack_a, stack_b, 1);
 	}
+} */
+
+void	ft_sort_it(t_stack *stack_a, t_stack *stack_b)
+{
+	int	index;
+
+	(void)index;
+	int i = 0;
+	while (i < stack_b->size)
+	{
+		printf("Stack B after sort: %d\n", stack_b->arr[i]);
+		i++;
+	}
+	//ft_pa_pb(stack_a, stack_b, 2); // Push two elements in stack B or push Just one, why push two? !!!!!!!!!!!!!!!!!!!!!!!!!!!!!11
+	//ft_pa_pb(stack_a, stack_b, 2); // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	while (stack_a->size > 2)
+	{
+		find_cheapest(stack_a, stack_b, 0);
+		printf("stack_b->size: %d <------> stack_a->size: %d\n", stack_b->size, stack_a->size);
+		printf("Zero problem: %d\n", stack_b->arr[0]);
+		ft_pa_pb(stack_a, stack_b, 2);
+		if (stack_b->size >= 2 && stack_b->arr[0] < stack_b->arr[1])
+			ft_sa_sb_ss(NULL, stack_b, 2);
+		else if (stack_b->size >= 2 && stack_b->arr[0] < stack_b->arr[1] && stack_b->arr[0] < stack_b->arr[stack_b->size - 1])
+			rotate(stack_b, NULL, 1, 1);	
+	}
+	if (stack_a->arr[0] > stack_a->arr[1])
+		ft_sa_sb_ss(stack_a, NULL, 1);	
+	// int i = 0;
+	// while (i < stack_b->size)
+	// {
+	// 	printf("Stack B after sort: %d\n", stack_b->arr[i]);
+	// 	i++;
+	// }
+	// while (stack_b->size > 0)
+	// {
+	// 	if (stack_b->size >= 2 && stack_b->arr[0] < stack_b->arr[1])
+	// 		ft_sa_sb_ss(NULL, stack_b, 2);
+	// 	index = find_min_max_index(stack_b, 1);
+	// 	rotate_min_max(stack_b, stack_a, index, 1);
+	// 	ft_pa_pb(stack_a, stack_b, 1);
+	// }
 }
 
 void	push_swap(t_stack *stack_a)
@@ -85,18 +127,15 @@ void	push_swap(t_stack *stack_a)
 	t_stack	*stack_b;
 	
 	stack_b = malloc(sizeof(t_stack));
+	stack_b->size = 0;
 	if (!stack_b)
 		return (ft_error(1));
 	if (stack_a->size == 2 && !ft_ifsorted(stack_a))
-	{
 		ft_sa_sb_ss(stack_a, NULL, 1);
-	}
 	else if (stack_a->size == 3 && !ft_ifsorted(stack_a))
 		ft_sort_three(stack_a);
 	else if (stack_a->size <= 5 && !ft_ifsorted(stack_a))
 		ft_sort_five(stack_a, stack_b);
 	else if (stack_a->size > 5 && !ft_ifsorted(stack_a))
-	{
 		ft_sort_it(stack_a, stack_b);
-	}
 }
