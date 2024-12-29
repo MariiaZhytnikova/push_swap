a66a7aaad643e3dceb371c58f5151d0519e24280
mzhitnik <119492493+MariiaZhytnikova@users.noreply.github.com>
2024-12-29 18:30:37 +0200

    Update ft_sort_utils.c

--- ft_sort_utils.c
+++ ft_sort_utils.c
@@ -12,51 +12,35 @@
 
 #include "push_swap.h"
 
-void	ft_smswap(t_stack *a, t_stack *b)
+void	rotate(t_stack *target, t_stack *stack, int direction, int a) 
 {
-	int	swap_a;
-	int	swap_b;
-
-	swap_a = (a->size >= 2 && a->arr[0] > a->arr[1]);
-	swap_b = (b->size >= 2 && b->arr[0] < b->arr[1]);
-	if (swap_a && swap_b)
+	int	val_target;
+	int	val_stack;
+	 // CASE B rotate_min_max(stack_b, stack_a, index, 1); // a = 1
+	 // CASE A rotate_min_max(stack_a, stack_b, index, 0); // a = 0
+	val_target = (target->size >= 2);	
+	if (a == 0 && direction > 0) // ANY WAY ROTATE A
 	{
-		ft_sa_sb_ss(a, b, 3);  // ss
-		printf("I use ft_smswap to sort\n"); // DELETE MEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEee
-	}
-	else if (swap_a)
-	{
-		ft_sa_sb_ss(a, NULL, 1);  // sa
-		printf("I use SA to sort\n"); // DELETE MEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEee
-	}
-	else if (swap_b)
-	{
-		ft_sa_sb_ss(NULL, b, 2);  // sb
-	}
-}
-
-void	sm_rotate(t_stack *target, t_stack *stack, int direction, int a) 
-{
-	if (direction > 0)
-	{
-		if (a == 0 && stack->size >= 2 && stack->arr[0] < stack->arr[1] && stack->arr[0] < stack->arr[stack->size - 1])
-		{
+		val_stack = (stack->size >= 2 && stack->arr[0] < stack->arr[1] && stack->arr[0] < stack->arr[stack->size - 1]);
+		if (val_stack && val_target)
 			ft_ra_rb_rr(target, stack, 3); // Perform combined rotation (rr)
-			printf("I use ft_smrotate to sort\n"); // DELETE MEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEee
-		}
-		else
-			ft_ra_rb_rr(target, NULL, 1); // rotate the TARGET stack anyway
+		else if (val_target)
+			ft_ra_rb_rr(target, stack, 1); // rotate the TARGET stack anyway
 	}
-	if (direction < 0)
+	if (a == 0 && direction < 0) // ANY WAY REVROTATE A
 	{
-		if (a == 0 && stack->size >= 2 && stack->arr[stack->size - 1] > stack->arr[1] && stack->arr[stack->size - 1] > stack->arr[0])
-		{
+		val_stack = (stack->size >= 2 && stack->arr[stack->size - 1] > stack->arr[0] && stack->arr[stack->size - 1] > stack->arr[stack->size - 2]);
+		if (val_stack && val_target)
 			ft_rra_rrb_rrr(target, stack, 3); // Perform combined rotation (rr)
-			printf("I use ft_smrotate to sort\n"); // DELETE MEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEee
-		}
-		else
-			ft_rra_rrb_rrr(target, NULL, 1); // rotate the TARGET stack anyway
+		else if (val_target)
+			ft_rra_rrb_rrr(target, stack, 1); // rotate the TARGET stack anyway
 	}
+	if (a == 1 && direction > 0)
+		if (val_target)
+			ft_ra_rb_rr(target, stack, 1); // rotate the TARGET stack anyway
+	if (a == 1 && direction < 0)
+		ft_rra_rrb_rrr(target, stack, 1); // rotate the TARGET stack anyway
+
 }
 
 void    rotate_min_max(t_stack *target, t_stack *stack, int index, int a)
@@ -64,13 +48,13 @@ void    rotate_min_max(t_stack *target, t_stack *stack, int index, int a)
     if (index <= target->size / 2) // If the index is closer to the top (less than half the stack size)
 	{
 		while (index-- > 0) // Perform forward rotations (ra)
-			sm_rotate(target, stack, 1, a);  // Perform ra: rotate the stack
+			rotate(target, stack, 1, a);  // Perform ra: rotate the stack
 	}
 	else
 	{
 		index = target->size - index;  // Convert index to the reverse distance from bottom
 		while (index-- > 0) // Perform reverse rotations (rra)
-			sm_rotate(target, stack, -1, a);  // Perform rra: reverse rotate the stack
+			rotate(target, stack, -1, a);  // Perform rra: reverse rotate the stack
 	}
 }
 
@@ -114,5 +98,6 @@ void	find_cheapest(t_stack *target, t_stack *stack, int a)
 		max_cost = size - max_pos; // If the element is closer to the bottom, reverse rotate (rra)
 	if (min_cost <= max_cost)
 		rotate_min_max (target, stack, min_pos, a);
-	rotate_min_max (target, stack, max_pos, a);
+	else
+		rotate_min_max (target, stack, max_pos, a);
 }
